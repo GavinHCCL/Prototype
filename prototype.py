f7df549962645ced2f32a3fd6313ed6f41d2bff8
GavinHCCL <89994967+GavinHCCL@users.noreply.github.com>
2022-12-20 20:27:10 -0800

    Update 0.1
    Changed it so it randomly sets the green and red dot instead of just the green dot
    It also now has a more wide area to draw on.

--- prototype.py
+++ prototype.py
@@ -4,9 +4,16 @@ import random
 # create a turtle object
 t = turtle.Turtle()
 
+# generate a random x and y coordinate
+x_red = random.randint(-380, 200)
+y_red = random.randint(-160, 250)
+
+x_green = random.randint(-380, 200)
+y_green = random.randint(-160, 250)
+
 # move the turtle to the starting position for the red dot
 t.penup()
-t.goto(-100, 0)
+t.goto(x_red, y_red)
 t.pendown()
 
 # set the turtle's color to red and draw a dot
@@ -17,12 +24,8 @@ t.dot()
 for i in range(20):
     t.forward(10)
 
-# generate a random x and y coordinate
-x = random.randint(-200, 200)
-y = random.randint(-200, 200)
-
 # move the turtle to the random position
-t.goto(x, y)
+t.goto(x_green, y_green)
 
 # set the turtle's color to green and draw a dot
 t.color("green")
